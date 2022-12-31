7749f0e598909aaf0a7eaa11eaac82687dbb54b6
Gavral <gavrilov-al@mail.ru>
2022-12-31 18:34:26 +0300

    3

--- pep_parse/items.py
+++ pep_parse/items.py
@@ -2,6 +2,6 @@ import scrapy
 
 
 class PepParseItem(scrapy.Item):
-    number = scrapy.Field()  # номер PEP
-    name = scrapy.Field()  # название PEP
-    status = scrapy.Field()  # статус, указанный на странице PEP
+    number = scrapy.Field()
+    name = scrapy.Field()
+    status = scrapy.Field()
--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -6,8 +6,8 @@ from datetime import datetime as dt
 
 DATETIME_FORMAT = '%Y-%m-%dT%H-%M-%S'
 DATETIME = dt.now().strftime(DATETIME_FORMAT)
-
 BASE_DIR = Path(__file__).parent.parent
+STATUS_SUMMARY = f'status_summary_{DATETIME}.csv'
 
 
 class PepParsePipeline:
@@ -20,7 +20,7 @@ class PepParsePipeline:
 
     def close_spider(self, spider):
         total = sum(self.pep_statuses.values())
-        report_file = BASE_DIR / 'results' / f'status_summary_{DATETIME}.csv'
+        report_file = BASE_DIR / 'results' / STATUS_SUMMARY
         with open(report_file, 'w', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(['Статус', 'Количество'])
